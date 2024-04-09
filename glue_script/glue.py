f41d2879d32b0614e762e7eabc3faa0f7f96bfaa
Kaushikdhola <kaushikdhola02@gmail.com>
2024-04-09 14:32:35 -0300

    refactor code

--- glue_script/glue.py
+++ glue_script/glue.py
@@ -20,10 +20,27 @@ job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
 # Script generated for node Amazon S3
-AmazonS3_node1712334626834 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, connection_type="s3", format="csv", connection_options={"paths": ["s3://reddit-extracted-raw-data/reddit-data/"], "recurse": True}, transformation_ctx="AmazonS3_node1712334626834")
+AmazonS3_node1712334626834 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", 
+                                        "withHeader": True, "separator": ",", 
+                                        "optimizePerformance": False}, 
+                                        connection_type="s3", format="csv", 
+                                        connection_options={"paths": ["s3://reddit-extracted-raw-data/reddit-data/"], 
+                                        "recurse": True}, 
+                                        transformation_ctx="AmazonS3_node1712334626834")
 
 # Script generated for node Change Schema
-ChangeSchema_node1712334666935 = ApplyMapping.apply(frame=AmazonS3_node1712334626834, mappings=[("title", "string", "title", "string"), ("score", "string", "score", "bigint"), ("num_comments", "string", "num_comments", "bigint"), ("created_utc", "string", "created_utc", "double"), ("upvote_ratio", "string", "upvote_ratio", "double"), ("award_count", "string", "award_count", "bigint"), ("number_of_upvotes", "string", "number_of_upvotes", "bigint"), ("number_of_downvotes", "string", "number_of_downvotes", "bigint"), ("text_content", "string", "text_content", "string"), ("is_video", "string", "is_video", "boolean")], transformation_ctx="ChangeSchema_node1712334666935")
+ChangeSchema_node1712334666935 = ApplyMapping.apply(frame=AmazonS3_node1712334626834, 
+                                mappings=[("title", "string", "title", "string"), 
+                                          ("score", "string", "score", "bigint"), 
+                                          ("num_comments", "string", "num_comments", "bigint"), 
+                                          ("created_utc", "string", "created_utc", "double"), 
+                                          ("upvote_ratio", "string", "upvote_ratio", "double"), 
+                                          ("award_count", "string", "award_count", "bigint"), 
+                                          ("number_of_upvotes", "string", "number_of_upvotes", "bigint"), 
+                                          ("number_of_downvotes", "string", "number_of_downvotes", "bigint"), 
+                                          ("text_content", "string", "text_content", "string"), 
+                                          ("is_video", "string", "is_video", "boolean")], 
+                                transformation_ctx="ChangeSchema_node1712334666935")
 
 # Create the 'reddit-data-parquet/' folder if it does not exist
 s3 = boto3.client('s3')
@@ -37,7 +54,13 @@ except KeyError:
     s3.put_object(Bucket=bucket_name, Key=(folder_name))
     
 # Script generated for node Amazon S3
-AmazonS3_node1712334675109 = glueContext.write_dynamic_frame.from_options(frame=ChangeSchema_node1712334666935, connection_type="s3", format="glueparquet", connection_options={"path": "s3://reddit-extracted-raw-data/reddit-data-parquet/", "partitionKeys": []}, format_options={"compression": "uncompressed"}, transformation_ctx="AmazonS3_node1712334675109")
+AmazonS3_node1712334675109 = glueContext.write_dynamic_frame.from_options(frame=ChangeSchema_node1712334666935, 
+                                        connection_type="s3", 
+                                        format="glueparquet", 
+                                        connection_options={"path": "s3://reddit-extracted-raw-data/reddit-data-parquet/", 
+                                        "partitionKeys": []}, 
+                                        format_options={"compression": "uncompressed"}, 
+                                        transformation_ctx="AmazonS3_node1712334675109")
 
 lambda_client = boto3.client('lambda')
 
